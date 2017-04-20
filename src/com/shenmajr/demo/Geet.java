044abce82029a353a391a444615876a4177b8d02
unknown <sh00716@shenmajr.com>
2017-04-20 16:56:34 +0800

    Initial commit

---
+++ src/com/shenmajr/demo/Geet.java
@@ -0,0 +1,13 @@
+package com.shenmajr.demo;
+
+/**
+ * Created by sh00716 on 2017/4/20.
+ */
+public class Geet {
+
+    public static void main(String[] args) {
+        System.out.println("helle world");
+
+    }
+
+}
